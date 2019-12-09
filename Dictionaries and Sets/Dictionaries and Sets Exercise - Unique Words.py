140ff7290a2b8c00a2a99c95e8c36f5996ee5f3e
lkmflam <thereallexusmurray@gmail.com>
2019-12-09 11:31:59 -0800

    9 DEC More Practice. Unfinished

---
+++ Dictionaries and Sets/Dictionaries and Sets Exercise - Unique Words.py
@@ -0,0 +1,6 @@
+opening_file= open("words.txt", "r")
+unique_list = []
+readinglines = fileopen.readlines()
+unique_words = set(word_list)
+for word in unique_words:
+    file.write(str(word) + "\n")
