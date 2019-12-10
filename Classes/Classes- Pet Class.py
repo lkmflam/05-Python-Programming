9e6159b0c428a0b07e4b0a5e2aa3fca2e4a0562a
lkmflam <thereallexusmurray@gmail.com>
2019-12-10 12:58:37 -0800

    10 DEC more practice needed

--- Classes/Classes- Pet Class.py
+++ Classes/Classes- Pet Class.py
@@ -3,5 +3,19 @@ class Pet:
         self.name = name
         self.animaltype = animaltype
         self.age = age
-    def setname(self):
+
+ '''  def description(self):
         return "{} is {} years old".format(self.name, self.age)
+
+class Doberman(Pet):
+    def run(self, speed):
+        return "{} runs {}".format(self.name, speed)
+        print(Pet)
+'''
+#dog1 = Pet("Pete")
+#dog1.name
+
+    #def setname(self):
+     #   return "{} is {} years old".format(self.name, self.age)
+        #Ask questions about the get and set command stated in the
+        #exercise material. Looks differet from the dcumentation.
